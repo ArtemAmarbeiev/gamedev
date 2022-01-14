0a86fe317b039ff92f8acdb5c16dd9be1052652b
ArtemAmarbeiev <93152562+ArtemAmarbeiev@users.noreply.github.com>
2022-01-14 15:19:05 +0200

    Update game.py
    def main

--- game.py
+++ game.py
@@ -69,4 +69,75 @@ def redraw_window(players, balls, game_time, score):
 	# draw score
 	text = TIME_FONT.render("Score: " + str(round(score)),1,(0,0,0))
 	WIN.blit(text,(10,15 + text.get_height()))
+def main(name):
+	global players
 
+	server = Network()
+	current_id = server.connect(name)
+	balls, players, game_time = server.send("get")
+
+	clock = pygame.time.Clock()
+
+	run = True
+	while run:
+		clock.tick(30) # 30 fps max
+		player = players[current_id]
+		vel = START_VEL - round(player["score"]/14)
+		if vel <= 1:
+			vel = 1
+
+		keys = pygame.key.get_pressed()
+
+		data = ""
+		if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+			if player["x"] - vel - PLAYER_RADIUS - player["score"] >= 0:
+				player["x"] = player["x"] - vel
+
+		if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+			if player["x"] + vel + PLAYER_RADIUS + player["score"] <= W:
+				player["x"] = player["x"] + vel
+
+		if keys[pygame.K_UP] or keys[pygame.K_w]:
+			if player["y"] - vel - PLAYER_RADIUS - player["score"] >= 0:
+				player["y"] = player["y"] - vel
+
+		if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+			if player["y"] + vel + PLAYER_RADIUS + player["score"] <= H:
+				player["y"] = player["y"] + vel
+
+		data = "move " + str(player["x"]) + " " + str(player["y"])
+
+		balls, players, game_time = server.send(data)
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					run = False
+
+
+		redraw_window(players, balls, game_time, player["score"])
+		pygame.display.update()
+
+
+	server.disconnect()
+	pygame.quit()
+	quit()
+
+
+while True:
+ 	name = input("Please enter your name: ")
+ 	if  0 < len(name) < 20:
+ 		break
+ 	else:
+ 		print("Error, this name is not allowed (must be between 1 and 19 characters [inclusive])")
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,30)
+
+# setup pygame window
+WIN = pygame.display.set_mode((W,H))
+pygame.display.set_caption("Blobs")
+
+main(name)
